6296deb6a9bba749681cea3f87cdc605e5efffc1
Raylan Campos <raylan@raylansoares.com>
2020-04-25 18:30:22 -0300

    Add subscribers routes for findOne, create and update

--- app/routes/subscribers.js
+++ app/routes/subscribers.js
@@ -1,11 +1,34 @@
 import express from 'express';
-import { find as findSubscribers } from '../services/subscribers'
+import { 
+	find as findSubscribers,
+	create as createSubscriber,
+	findOne as findOneSubscriber,
+	update as updateSubscriber,
+} from '../services/subscribers'
 
 const router = express.Router();
 
 router.route('/')
 	.get(async (req, res) => {
 		const data = await findSubscribers()
+		res.json(data);
+	})
+
+router.route('/')
+	.post(async (req, res) => {
+		const data = await createSubscriber(req.body)
+		res.json(data);
+	})
+
+router.route('/:id')
+	.get(async (req, res) => {
+		const data = await findOneSubscriber(req.params.id)
+		res.json(data);
+	})
+
+router.route('/:id')
+	.patch(async (req, res) => {
+		const data = await updateSubscriber(req.params.id, req.body)
 		res.json(data);
 	})
 
