77b4fdd916d52aadcd85e1234318ee80f1af1e6a
Raylan Campos <raylan@raylansoares.com>
2020-04-25 16:21:53 -0300

    Add prizes service

---
+++ app/services/prizes.js
@@ -0,0 +1,32 @@
+import moment from 'moment'
+import Prize from '../models/prize'
+
+const find = async () => {
+    const prizes = await Prize.find({}).sort('-created_at');
+    return prizes
+}
+
+const create = async (body) => {
+    body.created_at =  moment()
+    body.updated_at =  moment()
+    const prize = await Prize.create(body);
+    return prize
+}
+
+const update = async (_id, body) => {
+    body.updated_at =  moment()
+    const prize = await Prize.findOneAndUpdate({ _id: _id}, body);
+    return prize
+}
+
+const findOne = async (_id) => {
+    const prize = await Prize.findById(_id);
+    return prize
+}
+
+export {
+    find,
+    create,
+    update,
+    findOne
+}
