6c15dc7c767163f90e5b4896d39e6ad4caadd9c4
Raylan Campos <raylan@raylansoares.com>
2020-04-25 16:23:10 -0300

    Fix typo

--- app/routes/subscribers.js
+++ app/routes/subscribers.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { find as findSubscriber } from '../services/subscribers'
+import { find as findSubscribers } from '../services/subscribers'
 
 const router = express.Router();
 
 router.route('/')
 	.get(async (req, res) => {
-		const data = await findSubscriber()
+		const data = await findSubscribers()
 		res.json(data);
 	})
 
