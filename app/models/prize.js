a6fd3d23c32eb046ce5a7fe350ad0c8bb99cab1a
Raylan Campos <raylan@raylansoares.com>
2020-04-25 16:19:59 -0300

    Add color property to prize

--- app/models/prize.js
+++ app/models/prize.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const PrizeSchema = new mongoose.Schema({
 	name: String,
 	description: String,
+	color: String,
 	enabled: Boolean,
 	created_at: Date,
 	updated_at: Date
