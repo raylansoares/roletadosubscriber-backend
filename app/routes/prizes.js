80f53c3dad127b65648aac02d30a0ae5b38aa471
Raylan Campos <raylan@raylansoares.com>
2020-04-25 18:26:57 -0300

    Add prizes routes for findOne, create and update

--- app/routes/prizes.js
+++ app/routes/prizes.js
@@ -1,11 +1,34 @@
 import express from 'express';
-import { find as findPrizes } from '../services/prizes'
+import { 
+	find as findPrizes,
+	create as createPrize,
+	findOne as findOnePrize,
+	update as updatePrize,
+} from '../services/prizes'
 
 const router = express.Router();
 
 router.route('/')
 	.get(async (req, res) => {
 		const data = await findPrizes()
+		res.json(data);
+	})
+
+router.route('/')
+	.post(async (req, res) => {
+		const data = await createPrize(req.body)
+		res.json(data);
+	})
+
+router.route('/:id')
+	.get(async (req, res) => {
+		const data = await findOnePrize(req.params.id)
+		res.json(data);
+	})
+
+router.route('/:id')
+	.patch(async (req, res) => {
+		const data = await updatePrize(req.params.id, req.body)
 		res.json(data);
 	})
 
