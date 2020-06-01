f136ba21982b5effd8ccdd06ef7e2706df05bd61
Raylan Campos <raylan@raylansoares.com>
2020-06-01 20:31:15 -0300

    Add get users route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import prizeRoutes from './prizes';
 import subscriberRoutes from './subscribers';
 import authRoutes from './auth';
+import userRoutes from './user';
 
 const router = express.Router();
 
 router.use('/prizes', prizeRoutes);
 router.use('/subscribers', subscriberRoutes);
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 
 export default router;
---
+++ app/routes/user.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { 
+	find as findUsers,
+} from '../services/users'
+
+const router = express.Router();
+
+router.use(async (req, res, next) => {
+	if (!req.headers['x-client-secret'] || req.headers['x-client-secret'] !== process.env.CLIENT_SECRET) {
+		res.sendStatus(401)
+		return false
+	}
+	next()
+  })
+
+router.route('/')
+	.get(async (req, res) => {
+		const data = await findUsers()
+		res.json(data);
+	})
+
+export default router;
